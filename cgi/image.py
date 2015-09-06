93ce2ff243cdbf55e66ce6eabde7b7e7da0ca5e0
Annubis45 <annubis45@gmail.com>
2015-09-06 18:47:20 +0200

    v2

--- cgi/image.py
+++ cgi/image.py
@@ -4,14 +4,21 @@
 import cgi 
 import base64
 import sys
+import requests, json
 
 
+query = json.dumps(
+{
+	"query": { "match_all": {} }
+})
+response = requests.get("http://localhost:9200/photodisplayer/current/photo")
+results = json.loads(response.text)
 
-form = cgi.FieldStorage()
-file1 = form.getvalue("image")
 
-data = open(file1, 'rb').read()
+fullpath = results["_source"]["fullpath"]
 
+#form = cgi.FieldStorage()
+#file1 = form.getvalue("image")
+data = open(fullpath, 'rb').read()
 sys.stdout.buffer.write(b"Content-Type: image/jpeg\n\n") 
 sys.stdout.buffer.write(data)
-
--- cgi/index.py
+++ cgi/index.py
@@ -3,44 +3,109 @@
 
 import cgi 
 import base64
+import random
+import requests, json
+
+def add_note(ajout) :
+	headers = {'content-type' : 'application/json'}
+	response = requests.get("http://localhost:9200/photodisplayer/current/photo")
+	results = json.loads(response.text)
+	note = results["_source"]["note"]
+	results["_source"]["note"] = note+ajout
+	returnJson=results["_source"]
+	query2 = json.dumps(returnJson)
+	url="http://localhost:9200/photodisplayer/photo/"+results["_source"]["id"];
+	response2 = requests.put(url, data=query2)
+	print(json.loads(response2.text))
+
+def ban():
+	headers = {'content-type' : 'application/json'}
+	response = requests.get("http://localhost:9200/photodisplayer/current/photo")
+	results = json.loads(response.text)
+	note = results["_source"]["note"]
+	results["_source"]["note"] = 0
+	returnJson=results["_source"]
+	query2 = json.dumps(returnJson)
+	url="http://localhost:9200/photodisplayer/photo/"+results["_source"]["id"];
+	response2 = requests.put(url, data=query2)
+
+def getRandom():
+	query = json.dumps(
+	{
+	"query": {
+	"function_score": {
+	"functions": [
+	{
+	"random_score": {},
+	"weight": 1
+	},
+	{
+	"field_value_factor": {
+	"field": "note"
+	},
+	"weight": 1
+	}
+	],
+	"score_mode": "multiply"
+	}
+	}
+	})
+	response = requests.get("http://localhost:9200/photodisplayer/photo/_search?size=1", data=query)
+	results = json.loads(response.text)
+	returnJson=results["hits"]["hits"][0]["_source"]
+	query2 = json.dumps(returnJson)
+	url="http://localhost:9200/photodisplayer/current/photo";
+	response2 = requests.put(url, data=query2)
+	
+	
 
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
 
-
-
-#print(image)
+#Init
 var = 0;
-photos = ["DSC_0272.JPG", "DSC_0269.JPG", "DSC_02XX.JPG"]
+html = ""
+
+
+
+
+#Get the order
 action = form.getvalue("action")
+idcurrent = form.getvalue("id")
+idprevious = form.getvalue("previous")
 
-image = ""
-
+#Switch "order"
 if action=="ban":
-	image="ok"
+	ban()
+	html="ok"
 
 if action=="next":
-	var = var+1
-	ind = ((var)%len(photos))
-	image="image.py?image="+photos[ind]
+	getRandom();
+	html=random.random()*random.random();
 
 if action=="previous":
-	var = var-1
-	ind = ((var)%len(photos))
-	image="image.py?image="+photos[ind]
-
+	getRandom();
+	html=random.random()*random.random();
 
 if action=="like":
-	image="ok"
+	add_note(1)
+	html="ok"
 
 if action=="dislike":
-	image="ok"
+	add_note(-1)
+	html="ok"
 
 if action=="" or str(action) == "None":
+	getRandom();
 	mon_fichier = open("main.html", "r")
 	contenu = mon_fichier.read()
-	image=contenu
+	html=contenu
+	
 
+#Return the content
 #print("<!--" +str(action) +"-->")
-print(image)
+print(html)
+
+
+
 
