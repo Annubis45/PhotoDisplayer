100eeca188d2d198e601e0a3f90601c14e010d86
Annubis45 <annubis45@gmail.com>
2016-08-11 11:55:53 +0200

    Simplification

--- cgi/image.py
+++ cgi/image.py
@@ -9,20 +9,11 @@ import os.path
 form = cgi.FieldStorage()
 idcurrent = form.getvalue("idcurrent")
 
-mock = False
-if idcurrent=="" or type(idcurrent)==type(None) or idcurrent=="0":
-	mock =True
-else:
-	imageRaw = requests.get("http://localhost:9200/photodisplayer/photo/"+idcurrent)
-	imageJson = json.loads(imageRaw.text)
-	fullpath = imageJson["_source"]["fullpath"]
-	if os.path.exists(fullpath) :
-		data = open(fullpath, 'rb').read()
-	else:
-		mock=True
-
-if mock:
-	data = open("DSC_0269.JPG", 'rb').read()
+imageRaw = requests.get("http://localhost:9200/photodisplayer/photo/"+idcurrent)
+imageJson = json.loads(imageRaw.text)
+fullpath = imageJson["_source"]["fullpath"]
+if os.path.exists(fullpath) :
+	data = open(fullpath, 'rb').read()
 
 sys.stdout.buffer.write(b"Content-Type: image/jpeg\n\n") 
 sys.stdout.buffer.write(data)
